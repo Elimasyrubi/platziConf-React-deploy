d4a0b7988b48539148f4d58c54d93c8175dcaf6b
Eliezer Mas y rubi <elimasyrubi@gmail.com>
2020-04-09 12:23:22 -0300

    Updates

---
+++ precache-manifest.d66c34eec9ebb4cb6a676262411c38ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea1ede4291cdc0c7aecb",
+    "url": "/platziConf-React-deploy/static/css/main.01d601ea.chunk.css"
+  },
+  {
+    "revision": "ea1ede4291cdc0c7aecb",
+    "url": "/platziConf-React-deploy/static/js/main.ea1ede42.chunk.js"
+  },
+  {
+    "revision": "2a9139cbf1c128d67fb8",
+    "url": "/platziConf-React-deploy/static/js/runtime~main.2a9139cb.js"
+  },
+  {
+    "revision": "ae7c1783c8c163fe6279",
+    "url": "/platziConf-React-deploy/static/css/2.53bddb6d.chunk.css"
+  },
+  {
+    "revision": "ae7c1783c8c163fe6279",
+    "url": "/platziConf-React-deploy/static/js/2.ae7c1783.chunk.js"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/platziConf-React-deploy/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/platziConf-React-deploy/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/platziConf-React-deploy/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/platziConf-React-deploy/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platziConf-React-deploy/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "fc00b49b0337e1503fa0839c50ac079e",
+    "url": "/platziConf-React-deploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/platziConf-React/precache-manifest.ac91ff5486c7172a8e0621994d3a23d2.js"
+  "/platziConf-React-deploy/precache-manifest.d66c34eec9ebb4cb6a676262411c38ca.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/platziConf-React/index.html", {
+workbox.routing.registerNavigationRoute("/platziConf-React-deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
